26f05acddedcfc18e8ea21c6cb098e4a973dfd2a
AL22F13 <cotadmc@gmail.com>
2015-04-29 14:53:11 -0500

    actualizacion
    cosas nuevas

--- js/acciones.js
+++ js/acciones.js
@@ -1,14 +1,19 @@
 // JavaScript Document
 $(document).ready(function(e) {
-document.addEventListener("deviceready",function(){
-$("#btnvibrar").on("tap",function(){navigator.vibrate(1000);}); 
+document.addEventListener('deviceready',function(){
+$('#btnvibrar').on('tap',function(){
+	navigator.vibrate(1000);}); 
 //tap a btnvibrar
-$("#btnbeep").on("tap",function(){navigator.beep(1);});
+
+$('#btnbeep').on('tap',function(){
+	navigator.notification.beep(1);});
 //tap a btnbeep
-$("#Izquierda").on("swipeleft",function(){alert("Barrio a la Izquierda")
-;});
-$("#Derecha").on("swiperight",function(){alert("Barrio a la Derecha")
-;});
+
+$('#Izquierda').on('swipeleft',function(){
+	alert("Barrio a la Izquierda");});
+	
+$('#Derecha').on('swiperight',function(){
+	alert("Barrio a la Derecha");});
 
 }); 
 });
